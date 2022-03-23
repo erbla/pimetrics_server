e6a4c138c8e50c802bab5ef0138ec148e036ef80
Eric Blais <er.blais@gmail.com>
2022-03-23 18:49:36 -0400

    time has post and get methods now

--- app.py
+++ app.py
@@ -5,11 +5,16 @@ app = Flask(__name__)
 def index():
     with open('last_ping.dat', 'r') as f:
         lastping = f.readline()
-    return f'<h1>Last ping was: {lastping}</h1>'
+    return f'Last ping was: {lastping}'
 
-@app.route('/time', methods=['POST'])
+@app.route('/time', methods=['GET','POST'])
 def set_time():
-    lastping = request.form['lastping']
-    with open('last_ping.dat', 'w') as f:
-        f.write(lastping)
-    return lastping
+    if request.method == 'POST':
+        lastping = request.form['lastping']
+        with open('last_ping.dat', 'w') as f:
+            f.write(lastping)
+        return lastping
+    else:
+        with open('last_ping.dat', 'r') as f:
+            lastping = f.readline()
+        return {'lastping': lastping}
